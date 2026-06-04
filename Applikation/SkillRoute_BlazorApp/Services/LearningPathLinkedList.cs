34a4cedd522dee3ca84cf78c89c7c6da468d3bee
Ciprian Birou <Bir210617@spengergasse.at>
2026-06-04 22:42:17 +0200

    ADD: learning path

---
+++ Applikation/SkillRoute_BlazorApp/Services/LearningPathLinkedList.cs
@@ -0,0 +1,38 @@
+﻿namespace SkillRoute_BlazorApp.Services;
+
+public class LearningPathLinkedList
+{
+    private LearningPathNode? head;
+
+    public int Count { get; private set; }
+
+    public void AddLast(LearningPathNode node)
+    {
+        if (head is null)
+        {
+            head = node;
+        }
+        else
+        {
+            var current = head;
+            while (current.NextNode is not null)
+            {
+                current = current.NextNode;
+            }
+            current.NextNode = node;
+        }
+        Count++;
+    }
+
+    public List<LearningPathNode> ToList()
+    {
+        var result = new List<LearningPathNode>();
+        var current = head;
+        while (current is not null)
+        {
+            result.Add(current);
+            current = current.NextNode;
+        }
+        return result;
+    }
+}
---
+++ Applikation/SkillRoute_BlazorApp/Services/LearningPathNode.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillRoute_BlazorApp.Services;
+
+public class LearningPathNode
+{
+    public int BlockId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public LearningPathNode? NextNode { get; set; }
+}
